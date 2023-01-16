f4c12ab173151e642b6e507a870d8a335f85cd5c
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-16 15:40:09 +0100

    modif

--- generic/__init__.py
+++ generic/__init__.py
@@ -2,8 +2,8 @@ from typing import List, Tuple, Type, TypeAlias, Union
 
 Intervales_type: TypeAlias = List[Tuple[int, int]]
 Lenght_type: TypeAlias = int
-Type_type: TypeAlias = Union[int, str]
 
 
-def generate(intervales: Intervales_type):
+def generate(lenght: Lenght_type,
+             intervales: Intervales_type):
     pass
