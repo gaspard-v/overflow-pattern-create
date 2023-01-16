89012ca33c9cd9d20abbffaf3d650a4962132213
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-16 17:30:34 +0100

    modif

--- generic/__init__.py
+++ generic/__init__.py
@@ -9,15 +9,16 @@ def divide_chunks(my_list: List, n: int):
     return [my_list[i * n:(i + 1) * n] for i in range((len(my_list) + n - 1) // n)]
 
 
-def generate(lenght: Length_type, pattern_lenght: Length_type, intervales: Intervales_type):
+def generate(lenght: Length_type, pattern_length: Length_type, intervales: Intervales_type):
     number_intervales = len(intervales)
-    sets_bounds = [[(0, 0)]*pattern_lenght]*number_intervales
+    sets_bounds = []
     for index_i, (low, high) in enumerate(intervales):
-        offset = (high - low) // pattern_lenght
+        offset = (high - low) // pattern_length
         low_set = low
         high_set = low+offset
-        for p in range(pattern_lenght):
-            sets_bounds[index_i][p] = (low_set, high_set)
+        sets_bounds.append([])
+        for p in range(pattern_length):
+            sets_bounds[index_i].append((low_set, high_set))
             low_set = high_set
             high_set = high_set+offset
     pass
