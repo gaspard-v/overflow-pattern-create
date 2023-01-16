669fa08aa262c4bae12af048b462a5067a97d4e7
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-16 17:05:20 +0100

    path modification

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import x86.x64 as x64
 
 
 def main():
-    print(__name__)
+    x64.generate()
 
 
 if __name__ == "__main__":
--- x86/x64/__init__.py
+++ x86/x64/__init__.py
@@ -1,2 +1,8 @@
+import generic
+
 X86_64_LOW_RANGE = (0x0000_0000_0000_0000, 0x0000_7FFF_FFFF_FFFF)
 X86_64_HIGH_RANGE = (0xFFFF_8000_0000_0000, 0xFFFF_FFFF_FFFF_FFFF)
+
+
+def generate(*args, **kwargs):
+    return generic.generate(100, 8, [X86_64_LOW_RANGE, X86_64_HIGH_RANGE])
