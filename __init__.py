7b83a291c9a504a36aab82783732b0d21374a0ed
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-16 15:33:01 +0100

    add type in generic file

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-X86_64_LOW_RANGE = (0x0000_0000_0000_0000, 0x0000_7FFF_FFFF_FFFF)
-X86_64_HIGH_RANGE = (0xFFFF_8000_0000_0000, 0xFFFF_FFFF_FFFF_FFFF)
-
 
 def main():
     print(__name__)
---
+++ generic/__init__.py
@@ -0,0 +1,9 @@
+from typing import List, Tuple, Type, TypeAlias, Union
+
+Intervales_type: TypeAlias = List[Tuple[int, int]]
+Lenght_type: TypeAlias = int
+Type_type: TypeAlias = Union[int, str]
+
+
+def generate(intervales: Intervales_type):
+    pass
