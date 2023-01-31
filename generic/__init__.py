2488051968ea1cd8162db833cc55e4834aba17ee
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-31 08:23:02 +0100

    add "banned bytes" options

--- generic/__init__.py
+++ generic/__init__.py
@@ -3,17 +3,14 @@ from .utils import bytes_split_int
 import random
 
 
-def generate(length: int, pattern_length: int, address_ranges: list, allow_null_char: bool = False, reverse: bool = False) -> Set[Tuple[int]]:
+def generate(length: int, pattern_length: int, address_ranges: list, banned_bytes: list[int] = [0x0], reverse: bool = False) -> Set[Tuple[int]]:
     pattern_return = set()
     while len(pattern_return) < length:
         (low, high) = random.choice(address_ranges)
         random_number = random.randrange(low, high)
-        # TODO verifier si ce que retourne
-        # bytes_split_int est unique dans pattern_return
-        # unique aussi dans le sens inverse !
-        # use set.issuperset({tuple[::-1]})
-        pattern_return.add(
-            bytes_split_int(number=random_number,
-                            n=pattern_length, reverse=reverse, allow_null_char=allow_null_char)
-        )
+        s = bytes_split_int(number=random_number,
+                            n=pattern_length, reverse=reverse, banned_bytes=banned_bytes)
+        if pattern_return.issuperset({s[::-1]}):
+            continue
+        pattern_return.add(s)
     return pattern_return
--- generic/utils.py
+++ generic/utils.py
@@ -13,14 +13,14 @@ def get_bytes_in_int(n: int):
     return i
 
 
-def bytes_split_int(number: int, n: int, allow_null_char: bool = False, reverse: bool = False) -> Tuple[int]:
+def bytes_split_int(number: int, n: int, banned_bytes: list[int] = [0x0], reverse: bool = False) -> Tuple[int]:
     bytes_return = ()
     r = range(n)
     if reverse:
         r = range(n-1, -1, -1)
     for i in r:
         b = (number >> (8*i)) & 0xFF
-        if not b and not allow_null_char:
+        while not b or b in banned_bytes:
             b += 1
         bytes_return += (b,)
     return bytes_return
