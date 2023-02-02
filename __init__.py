1708686d7729e0191be4015997d6dc6fb973fe4e
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-02 08:52:08 +0100

    add exception

--- __init__.py
+++ __init__.py
@@ -3,6 +3,17 @@
 import x86.x64 as x64
 import generic.utils as utils
 import sys
+import argparse
+
+possible_
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog="overflow-pattern-create",
+                                     description="Create pattern for many architecture such as x86",
+                                     epilog="overflow-pattern-create")
+    parser.add_argument("--arch",
+                        help="")
 
 
 def main():
